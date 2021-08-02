9dad397324978d18fe470e17563ce06f5c2841a1
Hua_Nan <x565619@gmail.com>
2021-08-02 16:41:37 +0800

    Update 2021-08-02T08:41:31.805Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,15 +1,15 @@
+"use strict";
+
 // const focus = document.querySelectorAll('.sidebar-option');
-
 // focus.forEach(function(item) {
-
 //   item.addEventListener('click', function(e) {
 //    console.log(e);
 //   });
 // });
-
 $('.editor-show').on("click", function () {
   $('.editor').show();
 });
 $('.editor-hide').on("click", function () {
   $('.editor').hide();
 });
+//# sourceMappingURL=all.js.map
